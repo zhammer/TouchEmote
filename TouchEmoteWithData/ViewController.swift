903054bd694e6ca4765653e2ff8ae4ea7c6f4d75
Zach Hammer <zhammer@Zachs-Macbook-Pro.local>
2017-01-02 14:21:23 -0500

    Initial Commit

---
+++ TouchEmoteWithData/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchEmoteWithData
+//
+//  Created by Zach Hammer on 1/2/17.
+//  Copyright © 2017 Zach Hammer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
